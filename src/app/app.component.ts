bbbfd19020f30d95911aa97d623573cfbaf2fb8b
Olufemi Oyewole <femioyewole03@gmail.com>
2020-10-27 21:34:06 -0400

    feat: add layout for invoice creation steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'invoice-creation-ui';
+export class AppComponent implements OnDestroy {
+  invoiceFormGroup: FormGroup = this.fb.group({
+    description: new FormControl('', Validators.required),
+  });
+  isBigScreen = false;
+  smallScreenBreakpointObserver: Subscription;
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private fb: FormBuilder
+  ) {
+    this.smallScreenBreakpointObserver = this.breakpointObserver
+      .observe('(min-width: 599px)')
+      .subscribe((breakPointState) => {
+        this.isBigScreen = breakPointState.matches;
+      });
+  }
+
+  ngOnDestroy() {
+    this.smallScreenBreakpointObserver?.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    MatInputModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatToolbarModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
