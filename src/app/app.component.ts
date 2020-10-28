72b91c96de2b662994020b1c6314e80652add867
Olufemi Oyewole <femioyewole03@gmail.com>
2020-10-28 02:36:26 -0400

    feat: add required form fields in mobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
-import {
-  FormGroup,
-  FormBuilder,
-  FormControl,
-  Validators,
-} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { Subscription } from 'rxjs';
 
@@ -15,7 +10,20 @@ import { Subscription } from 'rxjs';
 })
 export class AppComponent implements OnDestroy {
   invoiceFormGroup: FormGroup = this.fb.group({
-    description: new FormControl('', Validators.required),
+    billedTo: ['', Validators.required],
+    lineItems: this.fb.array([
+      this.fb.group({
+        description: [undefined, Validators.required],
+        unitCost: [undefined, Validators.required],
+        quantity: [1, [Validators.required, Validators.min(1)]],
+      }),
+      this.fb.group({
+        description: [undefined, Validators.required],
+        unitCost: [undefined, Validators.required],
+        quantity: [1, [Validators.required, Validators.min(1)]],
+      }),
+    ]),
+    taxRate: [''],
   });
   isBigScreen = false;
   smallScreenBreakpointObserver: Subscription;
@@ -29,6 +37,27 @@ export class AppComponent implements OnDestroy {
       .subscribe((breakPointState) => {
         this.isBigScreen = breakPointState.matches;
       });
+  }
+
+  get lineItems() {
+    return this.invoiceFormGroup.get('lineItems') as FormArray;
+  }
+
+  get subtotal() {
+    let subtotal = 0;
+
+    this.lineItems.controls.forEach((_, index) => {
+      subtotal +=
+        this.lineItems.get([index])?.get('unitCost')?.value *
+        this.lineItems.get([index])?.get('quantity')?.value;
+    });
+
+    return subtotal;
+  }
+
+  get Total() {
+    const total = this.subtotal + this.invoiceFormGroup.get('taxRate')?.value;
+    return total;
   }
 
   ngOnDestroy() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -17,7 +19,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatStepperModule,
     MatButtonModule,
     MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
     ReactiveFormsModule,
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
