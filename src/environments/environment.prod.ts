acf5fded78630cd297086f598b09003a80993b10
Olufemi Oyewole <femioyewole03@gmail.com>
2020-10-30 14:22:02 -0400

    chore: add firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBybxBPQ9f1CtdZzVt3-NJHg9ZgSWNuFJU',
+    projectId: 'olufemi-oyewole-invoice-ui',
+  },
 };
